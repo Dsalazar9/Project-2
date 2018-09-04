8cbabff6a14945a3a5282092dbe692eac7767bbd
oscar <oscar@o2webdev.com>
2018-09-04 19:05:37 -0400

    clean up :lock:

--- server.js
+++ server.js
@@ -1,35 +1,37 @@
 // NPM Packages
 var express = require('express');
 var bodyParser = require('body-parser');
-var passport = require('./helpers/passport');
 var session = require('express-session');
-
+const db = require("./models");
 // Config
-var PORT = process.env.port || 8080;
+var passport = require('./helpers/passport');
+var secret = require('./config/keys');
+
+var PORT = process.env.PORT || 8080;
 var mode = process.env.NODE_ENV;
 var app = express();
 
-// Middleware
+// MiddleWare
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.use(sessionStorage({secret:"helloworld", resave:true, saveUnitialized: true}))
+// Auth & Session Initialization
+app.use(session({ secret: secret.key, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
 // Routes
 require('./routes/htmlRoutes')(app);
-// require('./routes/apiRoutes')(app);
+require('./routes/apiRoutes')(app);
 
+// Server
 db.sequelize.sync().then(() => {
     app.listen(PORT, function () {
-        if (mode !== 'production') {
-            var opn = require('opn');
-            opn(`http://localhost:${PORT}`, { app: ['google chrome'] })
-        }
+        // if (mode !== 'production') {
+        //     var opn = require('opn');
+        //     opn(`http://localhost:${PORT}`, { app: ['google chrome'] })
+        // }
         console.log(`👋  Hey there I'm 👂 ing on Port: ${PORT}`);
     });
 });
