9ca0af8c031b055ec2038c754775fe347a1f7b1d
oscar <oscar@o2webdev.com>
2018-09-06 08:31:01 -0400

    CleanUp

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/userData").then(function(data) {
-    $(".member-name").text(data.email+"| UserID: "+data.id);
+    // $(".member-name").text(data.email+"| UserID: "+data.id);
+    $(".member-name").text(data.email);
   });
 });
